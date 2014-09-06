b462a198076d651e909f56ef52ce80eef7b6a0ff
Mikhail Dubov <mdubov@gmail.com>
2014-09-06 12:17:41 +0100

    Remove angular

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "/sessions/current" => "ember_devise_simple_auth/sessions#show"
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'ember_devise_simple_auth/sessions' }
   resources :users
 end
