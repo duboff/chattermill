ccf251fbb4bbbb61bc889cf89ef764c2d167ea4d
Mikhail Dubov <mdubov@gmail.com>
2014-09-07 23:00:38 +0100

    Add fullname to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,20 @@
 describe User do
 
-  before(:each) { @user = User.new(email: 'user@example.com') }
+  before(:each) { @user = User.new(email: 'user@example.com', first_name: 'John', last_name: 'Watson') }
 
   subject { @user }
 
   it { should respond_to(:email) }
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
+  it { should respond_to(:full_name) }
 
   it "#email returns a string" do
     expect(@user.email).to match 'user@example.com'
+  end  
+
+  it "#full_name returns the right string" do
+    expect(@user.full_name).to match 'John Watson'
   end
 
 end
