04313422a68db7127bb5bbb0bdd3d20dd887c805
Mikhail Dubov <mdubov@gmail.com>
2014-09-30 00:23:30 +0100

    More request specs for companies

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 describe "Companies API" do
   describe "GET /company/:id" do
-    # after(:each) do
-    #  Warden.test_reset!
-    # end
-
     it "returns redicrect if not authorized" do
       company = create(:company)
 
@@ -14,19 +10,39 @@ describe "Companies API" do
       expect(response.status).to eq 302
     end
 
-    it 'returns a company if authorized' do
-      @user = create(:user, company: create(:company))
+    context 'authorized' do
 
-      sign_in_as_a_valid_user
-     
-      get "/api/v1/companies/#{@user.company_id}.json"
+      before do
+        @user = create(:user, company: create(:company))
+        sign_in_as_a_valid_user
+      end
 
-      expect(response.status).to eq 200
+      it 'returns a company if authorized' do     
+        get "/api/v1/companies/#{@user.company_id}.json"
 
-      json = JSON.parse(response.body).with_indifferent_access[:company]
+        expect(response.status).to eq 200
 
-      expect(json[:name]).to eq @user.company.name
-      expect(json[:website]).to eq @user.company.website
+        json = JSON.parse(response.body).with_indifferent_access[:company]
+
+        expect(json[:name]).to eq @user.company.name
+        expect(json[:website]).to eq @user.company.website
+      end
+
+      it 'company json embeds its users' do
+        get "/api/v1/companies/#{@user.company_id}.json"
+        json = JSON.parse(response.body).with_indifferent_access[:company]
+
+        expect(json[:user_ids]).to eq @user.company.users.map(&:id)
+      end
+
+      it 'company json embeds its projects' do
+        create(:project, company: @user.company)
+
+        get "/api/v1/companies/#{@user.company_id}.json"
+        json = JSON.parse(response.body).with_indifferent_access[:company]
+
+        expect(json[:project_ids]).to eq @user.company.projects.map(&:id)
+      end
     end
   end
 end
